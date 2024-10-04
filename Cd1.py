ae02fbb12ba6e1cf18f091b408951e17ba81559c
Heloyse Silva <sraheloysesantos@gmail.com>
2024-10-04 13:59:37 -0300

    Update Cd1.py

--- Cd1.py
+++ Cd1.py
@@ -1,9 +1,22 @@
-def main(logica):
-    operadores= []
-    
+def opr(n1, n2, operacoes):
+    if operacoes == '+':
+        return n1+n2 
+    if operacoes == '-':
+     return n1-n2
+    if operacoes == '/':
+     return n1/n2
+    if operacoes == '*':
+       return n1*n2
 
+def main():
+    while True:
+     n1= float(input('Primeiro numero:'))
+     n2= float(input('Segundo numero: '))
+     op= input('operação: ')
+     if op == '//':
+        break
+     operacoes= []
+     resultado= opr(n1=n1,n2=n2,operacoes=op)
+     print(resultado)
 
-def opr(operacoes):
-    n1= float(input('Primeiro numero:'))
-    n2= float(input('Segundo numero: '))
-    op= input('operação: ')
+main()
