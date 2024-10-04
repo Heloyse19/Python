b0f392e88ac496a1e7029c4ce986b7ea8cbefd49
Heloyse Silva <sraheloysesantos@gmail.com>
2024-10-04 14:48:27 -0300

    Update Cd1.py

--- Cd1.py
+++ Cd1.py
@@ -1,22 +1,39 @@
+# Definindo as operações
 def opr(n1, n2, operacoes):
     if operacoes == '+':
-        return n1+n2 
+        return f'{n1} + {n2} = {n1+n2}' 
     if operacoes == '-':
-     return n1-n2
+     return f'{n1} - {n2} = {n1-n2}'
     if operacoes == '/':
-     return n1/n2
+     return f'{n1} / {n2} = {n1/n2}'
     if operacoes == '*':
-       return n1*n2
+       return f'{n1} x {n2} = {n1*n2}'
+    if operacoes not in '+-/*':
+       return 'operação invalida'
+    
+# Definindo a lista de historico
+def historico(hist):
+   for lista in hist:
+      print(lista)
 
+# Principal
 def main():
+    operacoes= []
     while True:
-     n1= float(input('Primeiro numero:'))
-     n2= float(input('Segundo numero: '))
-     op= input('operação: ')
-     if op == '//':
-        break
-     operacoes= []
-     resultado= opr(n1=n1,n2=n2,operacoes=op)
-     print(resultado)
+        escolha = input('Calcular/Historico/Sair: ').capitalize()
+        if escolha == 'Calcular':
+            n1= float(input('Primeiro numero:'))
+            n2= float(input('Segundo numero: '))
+            op= input('operação: ')
+            resultado= opr(n1=n1,n2=n2,operacoes=op)
+        if escolha == 'Sair':
+            break
+
+        # Registrando o historico
+        operacoes.append(resultado)
+        if escolha == 'Historico':
+            historico(hist=operacoes)
+    
+        print(resultado)
 
 main()
